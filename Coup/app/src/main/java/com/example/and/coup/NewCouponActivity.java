232f892c4e204300800356a1608e6a0575b11167
liorsap1 <liorsap1@gmail.com>
2017-09-20 14:48:00 +0300

    j

---
+++ Coup/app/src/main/java/com/example/and/coup/NewCouponActivity.java
@@ -0,0 +1,42 @@
+package com.example.and.coup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+
+public class NewCouponActivity extends AppCompatActivity {
+
+
+    EditText couponName;
+    EditText couponId;
+    EditText couponProductPrice;
+    NumberPicker couponDiscount;
+    EditText couponDate;
+    Button submit,cancel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_coupon);
+        couponId = (EditText)findViewById(R.id.coupon_number);
+        couponName = (EditText)findViewById(R.id.coupon_name);
+        couponProductPrice = (EditText)findViewById(R.id.coupon_product_price);
+        couponDiscount = (NumberPicker)findViewById(R.id.coupon_discount);
+        couponDate = (EditText)findViewById(R.id.coupon_date);
+        submit = (Button) findViewById(R.id.coupon_submit);
+        cancel = (Button) findViewById(R.id.coupon_cancel);
+    }
+
+
+    private Coupon createCoupon(){
+        String id = couponId.getText().toString();
+        String productName = couponName.getText().toString();
+        String endDate = couponDate.getText().toString();
+        String discount = ""+couponDiscount.getValue();
+        String priceBefore = couponProductPrice.getText().toString();
+        String priceAfter = ""+;
+
+    }
+}
